c7dafdfc04e918a5a96a088b9341cb99cc0bf2d6
Jim Shaw <jimxshaw@gmail.com>
2018-05-01 20:50:08 -0500

    Add message passing example to main package.

--- client/main.go
+++ client/main.go
@@ -1,22 +1,51 @@
 package main
 
-import (
-	"github.com/jimxshaw/Gopay/payment"
-)
+import "fmt"
+
+//"github.com/jimxshaw/Gopay/payment"
+
+type CreditAccount struct{}
+
+func (c *CreditAccount) processPayment(amount float32) {
+	fmt.Println("Processing credit card payment...")
+}
+
+func CreateCreditAccount(chargeCh chan float32) *CreditAccount {
+	creditAccout := &CreditAccount{}
+
+	go func(chargeCh chan float32) {
+		for amount := range chargeCh {
+			creditAccout.processPayment(amount)
+		}
+	}(chargeCh)
+
+	return creditAccout
+}
 
 func main() {
-	var option payment.PaymentOption
+	// var option payment.PaymentOption
 
-	option = payment.CreateCreditAccount(
-		"James Madison",
-		"1111-2222-3333-4444",
-		7,
-		2030,
-		777)
+	// option = payment.CreateCreditAccount(
+	// 	"James Madison",
+	// 	"1111-2222-3333-4444",
+	// 	7,
+	// 	2030,
+	// 	777)
 
-	option.ProcessPayment(1000)
+	// option.ProcessPayment(1000)
 
-	option = payment.CreateCashAccount()
+	// option = payment.CreateCashAccount()
 
-	option.ProcessPayment(800)
+	// option.ProcessPayment(800)
+
+	/* MESSAGE PASSING */
+	chargeCh := make(chan float32)
+
+	CreateCreditAccount(chargeCh)
+
+	chargeCh <- 1000
+
+	// Prevent the main function from shutting down.
+	var a string
+	fmt.Scanln(&a)
 }
